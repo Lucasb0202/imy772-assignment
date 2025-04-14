703b68b835f78d828b4c6f3120ece509c12dc6fb
Lucasb0202 <lucab1602@gmail.com>
2025-04-14 15:01:17 +0200

    calculator ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  
+  const buttons = [
+    {
+      symbol: "0",
+    },
+    {
+      symbol: "1",
+    },
+    {
+      symbol: "2",
+    },
+    {
+      symbol: "3",
+    },
+    {
+      symbol: "4",
+    },
+    {
+      symbol: "5",
+    },
+    {
+      symbol: "6",
+    },
+    {
+      symbol: "7",
+    },
+    {
+      symbol: "8",
+    },
+    {
+      symbol: "9",
+    },
+    {
+      symbol: "A",
+    },
+    {
+      symbol: "B",
+    },
+    {
+      symbol: "C",
+    },
+    {
+      symbol: "D",
+    },
+    {
+      symbol: "E",
+    },
+    {
+      symbol: "F",
+    },
+    {
+      symbol: "AC"
+    }, 
+    {
+      symbol: "="
+    },
+    {
+      symbol: "+"
+    },
+    {
+      symbol: "-"
+    },
+    {
+      symbol: "X"
+    },
+    {
+      symbol: "÷"
+    }
+  ]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [displayValue, setDisplayValue] = useState("")
+  const handleButtonClick = (symbol) => {
+    if (symbol === "AC") {
+      setDisplayValue("")
+    } else {
+      setDisplayValue(displayValue + symbol)
+    }
+  }
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="border-2 border-white w-1/5 h-fit rounded-2xl bg-black overflow-hidden flex flex-col">
+        <div className="h-[100px] w-full text-white flex items-end justify-end px-4 py-2 text-3xl">
+          {displayValue}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="w-full px-2 py-2">
+          <div className="grid grid-cols-4 gap-2 h-full">
+            {buttons.map((item, index) => (
+              <button
+                key={index}
+                className={`cursor-pointer rounded-full ${item.symbol === "AC" || item.symbol == "=" || item.symbol == "X" || item.symbol == "÷" || item.symbol == "+" || item.symbol == "-" ? "bg-amber-600" : "bg-gray-800" } aspect-square w-full text-white text-xl`}
+                onClick={() => handleButtonClick(item.symbol)}
+              >
+                <span className="text-2xl">
+                  {item.symbol}
+                </span>
+              </button>
+            ))}
+          </div>
+      </div>
+  </div>
+</div>
   );
 }
